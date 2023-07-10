d9a2efeb296d8a292dc2ee0fbabc47ce7c10721b
Cihan <ozcancihan714@gmail.com>
2023-07-10 14:10:23 +0300

    Responsive navbar

--- src/components/Announcement.jsx
+++ src/components/Announcement.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Announcement = () => {
-  return <header className="bg-aqua font-urbanist font-bold text-white px-3 text-center">Super Deal! Free Shipping on Orders over $50</header>;
+  return <header className="bg-aqua font-urbanist font-bold text-white px-3 text-center md:text-base text-xs">Super Deal! Free Shipping on Orders over $50</header>;
 };
 
 export default Announcement;
--- src/containers/Navbar.jsx
+++ src/containers/Navbar.jsx
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { Announcement, Badge } from "../components";
 import { FcSearch } from "react-icons/fc";
 import { BsFillCartFill } from "react-icons/bs";
+import { AiOutlineMenu } from "react-icons/ai";
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     <div className="h-[60px] flex flex-col font-urbanist">
       <Announcement />
       <nav className="p-3 flex">
-        <div className="flex items-center w-1/3">
+        {/* Input */}
+        <div className="hidden md:flex items-center w-1/3">
           <span className="mr-5">EN</span>
 
           <div className="flex justify-center items-center border text-sm px-2 py-1">
@@ -17,15 +21,17 @@ const Navbar = () => {
           </div>
         </div>
 
-        <div className="w-1/3 flex justify-center items-center">
-          <legend className="font-bold text-4xl">TrendyMart</legend>
+        {/* LOGO */}
+        <div className="w-2/3 md:w-1/3 flex justify-center items-center">
+          <legend className="font-bold md:text-4xl text-2xl">TrendyMart</legend>
         </div>
 
-        <div className="w-1/3 flex justify-end items-center">
-          <a href="#" className="mr-2 text-sm">
+        {/* MENU */}
+        <div className="hidden md:flex w-1/3 flex justify-end items-center">
+          <a href="#" className="mr-4 text-sm">
             REGISTER
           </a>
-          <a href="#" className="mr-2 text-sm">
+          <a href="#" className="mr-4 text-sm">
             SIGN IN
           </a>
 
@@ -34,6 +40,28 @@ const Navbar = () => {
             <Badge>5</Badge>
           </a>
         </div>
+
+        {/* MENU BUTTON */}
+        <div className="md:hidden flex justify-center items-center w-1/3">
+          <AiOutlineMenu className="text-lg" onClick={() => setToggleMenu(prev => !prev)} />
+        </div>
+
+        {/* RESPONSIVE MENU */}
+        {toggleMenu && (
+          <div className="fixed flex flex-col justify-evenly items-center right-10 top-20 bg-aqua text-white w-1/2 h-1/4 rounded-lg ">
+            <a href="#" className="text-sm">
+              REGISTER
+            </a>
+            <a href="#" className="text-sm">
+              SIGN IN
+            </a>
+
+            <a href="#" className="text-lg relative">
+              <BsFillCartFill />
+              <Badge>5</Badge>
+            </a>
+          </div>
+        )}
       </nav>
     </div>
   );
