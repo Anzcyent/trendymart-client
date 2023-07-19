4fdf4e77530da80605183b8d3592bbde21e459c7
Cihan <ozcancihan714@gmail.com>
2023-07-19 14:12:24 +0300

    Continue Shopping Button Fix

--- src/containers/CartSection.jsx
+++ src/containers/CartSection.jsx
@@ -2,9 +2,11 @@ import React from "react";
 
 import { CartItem, CartSummary } from "../components";
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const CartSection = () => {
   const cart = useSelector((state) => state.cart);
+  const navigate = useNavigate();
 
   return (
     <div className="font-urbanist p-3 w-full">
@@ -13,6 +15,7 @@ const CartSection = () => {
         <button
           type="button"
           className="font-bold sm:text-base text-xs border-2 sm:px-3 px-1 sm:py-1 hover:bg-black hover:text-white active:scale-95 transition"
+          onClick={() => navigate("/")}
         >
           CONTINUE SHOPPING
         </button>
@@ -35,7 +38,9 @@ const CartSection = () => {
       <div className="flex sm:flex-row flex-col justify-around items-center">
         {/* PRODUCTS */}
         <div className="flex flex-col sm:w-2/3 w-full">
-          {cart.products.map(product => <CartItem key={product._id} product={product} />)}
+          {cart.products.map((product) => (
+            <CartItem key={product._id} product={product} />
+          ))}
         </div>
 
         {/* SUMMARY */}
